cb570c7a6a8b0232afee8707542838debd8389ad
koushik-913 <2420030232@klh.edu.in>
2025-08-04 10:35:11 +0530

    Update first.java

--- lab3/first.java
+++ lab3/first.java
@@ -1,37 +1,61 @@
 package lab3;
-
+import java.util.Scanner;
 public class first {
 
-	public static void main(String[] args) {
-		        String str1 = "listen";
-		        String str2 = "silent";
+		public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        String s1, s2;
 
-		        if (areAnagrams(str1, str2)) {
-		            System.out.println("Anagrams");
-		        } else {
-		            System.out.println("Not Anagrams");
-		        }
-		    }
+	        System.out.println("Enter first String: ");
+	        s1 = sc.nextLine();
+	        System.out.println("Enter second String: ");
+	        s2 = sc.nextLine();
 
-		    public static boolean areAnagrams(String str1, String str2) {
-		        str1 = str1.replaceAll("\\s", "").toLowerCase();
-		        str2 = str2.replaceAll("\\s", "").toLowerCase();
-		       if (str1.length() != str2.length()) {
-		            return false;
-		        }
-		        int[] count1 = new int[26];
-		        int[] count2 = new int[26];
-		        for (int i = 0; i < str1.length(); i++) {
-		            count1[str1.charAt(i) - 'a']++;
-		            count2[str2.charAt(i) - 'a']++;
-		        }
+	       /* s1 = s1.replaceAll("\\s", "").toLowerCase();
+	        s2 = s2.replaceAll("\\s", "").toLowerCase();*/
 
-		        for (int i = 0; i < 26; i++) {
-		            if (count1[i] != count2[i]) {
-		                return false;
-		            }
-		        }
+	        if (s1.length() != s2.length()) {
+	            System.out.println("Non-anagram");
+	            return;
+	        }
 
-		        return true;
-		    }
+	        char[] a = s1.toCharArray();
+	        char[] b = s2.toCharArray();
+
+	 
+	        for (int i = 0; i < a.length - 1; i++) {
+	            for (int j = 0; j < a.length - 1 - i; j++) {
+	                if (a[j] > a[j + 1]) {
+	                    char temp = a[j];
+	                    a[j] = a[j + 1];
+	                    a[j + 1] = temp;
+	                }
+	            }
+	        }
+
+	        for (int i = 0; i < b.length - 1; i++) {
+	            for (int j = 0; j < b.length - 1 - i; j++) {
+	                if (b[j] > b[j + 1]) {
+	                    char temp = b[j];
+	                    b[j] = b[j + 1];
+	                    b[j + 1] = temp;
+	                }
+	            }
+	        }
+
+	        boolean isAnagram = true;
+	        for (int i = 0; i < a.length; i++) {
+	            if (a[i] != b[i]) {
+	                isAnagram = false;
+	                break;
+	            }
+	        }
+
+	        if (isAnagram) {
+	            System.out.println("Anagram");
+	        } else {
+	            System.out.println("Non-anagram");
+	        }
 		}
+}
+	    
