af680bd639e9788c98366f3ad69d328d951634b0
koushik-913 <2420030232@klh.edu.in>
2025-08-06 09:07:47 +0530

    Update third.java

--- lab3/third.java
+++ lab3/third.java
@@ -1,10 +1,29 @@
 package lab3;
 
-public class third {
+import java.time.*;
+import java.time.format.*;
+import java.util.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class third
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter event date and time (yyyy-MM-dd HH:mm): ");
+		String s = sc.nextLine();
 
+		LocalDateTime e = LocalDateTime.parse(s, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+		LocalDateTime n = LocalDateTime.now();
+
+		if (e.isBefore(n)) {
+			System.out.println("The event  passed.");
+			
+		}
+
+		Duration d = Duration.between(n, e);
+		long m = d.toMinutes();
+
+		System.out.println("Time remaining ");
+		System.out.println((m / 1440) + " days, " + (m % 1440) / 60 + " hours, and " + (m % 60) + " minutes");
 	}
-
 }
